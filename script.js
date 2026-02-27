c23dbd864e74ce44e06af796fb1ee4e5088d925e
YannisLing <3270614976@qq.com>
2026-02-28 01:26:20 +0800

    完成主要页面和四则运算法则

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(a,b){
+  return a+b;
+}
+function sub(a,b){
+  return a-b;
+}
+function multi(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
+console.log(divide(1,3));
